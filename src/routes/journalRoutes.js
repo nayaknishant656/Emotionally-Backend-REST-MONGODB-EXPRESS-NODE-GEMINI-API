39b5a932b1620e99343380054aafef038d4dc73f
nayaknishant656 <nayaknishant656@gmail.com>
2026-03-15 15:01:07 +0530

    feat: Add routes for creating journals and managing journal analyses.

--- src/routes/journalRoutes.js
+++ src/routes/journalRoutes.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { getAllJournals, getJournalsByUserId, getJournalsByIdParam } = require('../controllers/journalController');
+const {
+    getAllJournals,
+    getJournalsByUserId,
+    getJournalsByIdParam,
+    createJournal,
+    getAnalysesByUserId,
+    createJournalAnalysis
+} = require('../controllers/journalController');
 
 /**
  * Journal Routes
  * Base path: /api/v1/journals
  */
 router.get('/', getAllJournals);
+router.post('/', createJournal);
 router.get('/user/:userId', getJournalsByUserId);
 router.get('/:id', getJournalsByIdParam);
+
+// Analysis routes
+router.get('/analysis/user/:userId', getAnalysesByUserId);
+router.post('/analysis', createJournalAnalysis);
 
 module.exports = router;
